37aa3eb74b0acea10792c06e6f60e33b222242d5
Arni Magnusson <thisisarni@gmail.com>
2023-09-27 09:34:44 +1100

    Add output.R

--- TAF/output.R
+++ TAF/output.R
@@ -1,9 +1,17 @@
 ## Extract results of interest, write TAF output tables
 
-## Before:
-## After:
+## Before: 11.par, catch.rep, length.fit, plot-11.par.rep, test_plot_output,
+##         weight.fit (model)
+## After:  11.par, catch.rep, length.fit, plot-11.par.rep, test_plot_output,
+##         weight.fit (output)
 
 library(TAF)
 
 mkdir("output")
 
+cp("model/11.par",           "output")
+cp("model/catch.rep",        "output")
+cp("model/length.fit",       "output")
+cp("model/plot-11.par.rep",  "output")
+cp("model/test_plot_output", "output")
+cp("model/weight.fit",       "output")
