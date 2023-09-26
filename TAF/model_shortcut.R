8c3b31d0adeb0ca079773d5df681d5c5e748461d
Arni Magnusson <thisisarni@gmail.com>
2023-09-27 09:33:58 +1100

    Add model_shortcut.R

---
+++ TAF/model_shortcut.R
@@ -0,0 +1,17 @@
+## Run analysis, write model results
+
+## Before: model_results (boot/data)
+## After:  11.par, catch.rep, length.fit, plot-11.par.rep, test_plot_output,
+##         weight.fit (model)
+
+library(TAF)
+
+mkdir("model")
+
+# Fetch model results
+cp("boot/data/model_results/11.par",           "model")
+cp("boot/data/model_results/catch.rep",        "model")
+cp("boot/data/model_results/length.fit",       "model")
+cp("boot/data/model_results/plot-11.par.rep",  "model")
+cp("boot/data/model_results/test_plot_output", "model")
+cp("boot/data/model_results/weight.fit",       "model")
