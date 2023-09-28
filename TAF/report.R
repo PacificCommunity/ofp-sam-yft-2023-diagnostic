7cbd2f6524e6b540902cb6f735b583e2fe69efbb
Arni Magnusson <thisisarni@gmail.com>
2023-09-29 01:09:54 +1100

    Round biology table

--- TAF/report.R
+++ TAF/report.R
@@ -1,25 +1,16 @@
 ## Prepare plots and tables for report
 
-## Before: 11.par, catch.rep, length.fit, plot-11.par.rep, test_plot_output,
-##         weight.fit (output)
+## Before: biology.csv, biomass.csv, catch.csv, cpue.csv, depletion.csv,
+##         fatage.csv, length.comps.csv, likelihoods.csv, natage.csv,
+##         selectivity.csv, stats.csv, weight.comps.csv (output)
 ## After:
 
 library(TAF)
-taf.library(FLR4MFCL)
 
 mkdir("report")
 
-# Read model output
-cat("Reading parameters ... ")
-par <- read.MFCLPar("output/11.par", first.yr=1952)
-cat("done\nReading catches ...\n")
-catch <- read.MFCLCatch("output/catch.rep", dimensions(par), range(par))
-cat("done\nReading length fits ... ")
-lenfit <- read.MFCLLenFit("output/length.fit")
-cat("done\nReading model estimates ... ")
-rep <- read.MFCLRep("output/plot-11.par.rep")
-cat("done\nReading likelihoods ... ")
-likelihood <- read.MFCLLikelihood("output/test_plot_output")
-cat("done\nReading weight fits ... ")
-wgtfit <- read.MFCLWgtFit("output/weight.fit")
-cat("done\n")
+biology <- read.taf("output/biology.csv")
+biology <- rnd(biology, 2:5, c(1,1,3,3))
+write.taf(biology, dir="report")
+
+biomass <- read.taf("output/biomass.csv")
