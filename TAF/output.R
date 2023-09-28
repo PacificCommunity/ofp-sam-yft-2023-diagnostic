9592d303d49e6436f05c3ae22b408132e229d547
Arni Magnusson <thisisarni@gmail.com>
2023-09-28 12:40:40 +1100

    Write TAF tables

--- TAF/output.R
+++ TAF/output.R
@@ -1,8 +1,10 @@
 ## Extract results of interest, write TAF output tables
 
-## Before: 11.par, catch.rep, length.fit, plot-11.par.rep, test_plot_output,
-##         weight.fit (model), fisheries.csv (data)
-## After:
+## Before: 00.par, 11.par, catch.rep, length.fit, plot-11.par.rep,
+##         test_plot_output, weight.fit (model), fisheries.csv (data)
+## After:  biology.csv, biomass.csv, catch.csv, cpue.csv, depletion.csv,
+##         fatage.csv, length.comps.csv, likelihoods.csv, natage.csv,
+##         selectivity.csv, stats.csv, weight.comps.csv (output)
 
 library(TAF)
 taf.library(FLR4MFCL)
@@ -40,28 +42,27 @@ hours <- file.mtime(finalPar("model")) - file.mtime("model/00.par")
 hours <- hours[[1]]
 stats <- data.frame(npar, objfun, gradient, start, hours)
 
-# Catches
+# Catch
 catch <- as.data.frame(fishery_catch(catches))
 catch$age <- catch$iter <- NULL
 names(catch)[names(catch) == "unit"] <- "fishery"
-names(catch)[names(catch) == "data"] <- "catch"
+names(catch)[names(catch) == "data"] <- "t"
 catch$area <- fisheries$area[catch$fishery]
+catch <- catch[c("year", "season", "fishery", "area", "t")]
 
 # Length comps
 length.comps <- lenfits(lenfit)
 length.comps$season <- (1 + length.comps$month) / 3
-length.comps$area <- fisheries$area[length.comps$fishery]
 names(length.comps)[names(length.comps) == "sample_size"] <- "ess"
-length.comps <- length.comps[c("year", "season", "fishery", "area",
-                               "ess", "length", "obs", "pred")]
+length.comps <- length.comps[c("year", "season", "fishery", "ess",
+                               "length", "obs", "pred")]
 
 # Weight comps
 weight.comps <- wgtfits(wgtfit)
 weight.comps$season <- (1 + weight.comps$month) / 3
-weight.comps$area <- fisheries$area[weight.comps$fishery]
 names(weight.comps)[names(weight.comps) == "sample_size"] <- "ess"
-weight.comps <- weight.comps[c("year", "season", "fishery", "area",
-                               "ess", "weight", "obs", "pred")]
+weight.comps <- weight.comps[c("year", "season", "fishery", "ess",
+                               "weight", "obs", "pred")]
 
 # Biology
 laa <- as.data.frame(mean_laa(rep))
@@ -75,4 +76,55 @@ biology$season <- NULL
 biology <- biology[order(biology$age),]
 biology$maturity <- mat(par)
 biology$natmort <- m_at_age(rep)
-rownames(biology) <- NULL
+
+# Selectivity
+selectivity <- as.data.frame(sel(rep))
+names(selectivity)[names(selectivity) == "unit"] <- "fishery"
+names(selectivity)[names(selectivity) == "data"] <- "sel"
+selectivity <- selectivity[c("fishery", "age", "sel")]
+
+# Fishing mortality
+fatage <- as.data.frame(fm(rep))
+names(fatage)[names(fatage) == "data"] <- "f"
+fatage <- fatage[c("year", "season", "area", "age", "f")]
+
+# Population
+natage <- as.data.frame(popN(rep))
+natage$unit <- natage$iter <- NULL
+names(natage)[names(natage) == "data"] <- "n"
+natage$n <- natage$n / 1e3
+natage <- natage[c("year", "season", "area", "age", "n")]
+
+# Spawning potential
+biomass <- as.data.frame(adultBiomass(rep))
+biomass$age <- biomass$unit <- biomass$iter <- NULL
+names(biomass)[names(biomass) == "data"] <- "ssb"
+biomass$ssb <- biomass$ssb / 1e3
+
+# Depletion
+depletion <- flr2taf(SBSBF0(rep))
+names(depletion) <- c("year", "dep")
+
+# CPUE
+obs <- as.data.frame(cpue_obs(rep))
+pred <- as.data.frame(cpue_pred(rep))
+names(obs)[names(obs) == "data"] <- "obs"
+names(pred)[names(pred) == "data"] <- "pred"
+cpue <- cbind(obs, pred["pred"])
+cpue <- cpue[cpue$unit %in% 33:37,]
+cpue$area <- as.integer(cpue$unit) - 32
+cpue$age <- cpue$unit <- cpue$iter <- NULL
+
+# Write TAF tables
+write.taf(likelihoods, dir="output")
+write.taf(stats, dir="output")
+write.taf(catch, dir="output")
+write.taf(length.comps, dir="output")
+write.taf(weight.comps, dir="output")
+write.taf(biology, dir="output")
+write.taf(selectivity, dir="output")
+write.taf(fatage, dir="output")
+write.taf(natage, dir="output")
+write.taf(biomass, dir="output")
+write.taf(depletion, dir="output")
+write.taf(cpue, dir="output")
