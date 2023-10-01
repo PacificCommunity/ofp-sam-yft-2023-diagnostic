e7444a9f3494562715c659df59f8bf4226ea1478
Arni Magnusson <thisisarni@gmail.com>
2023-10-01 19:30:33 +1100

    Calculate f.annual, f.aggregated f.stage

--- TAF/output.R
+++ TAF/output.R
@@ -2,9 +2,10 @@
 
 ## Before: 00.par, 11.par, catch.rep, length.fit, plot-11.par.rep,
 ##         test_plot_output, weight.fit (model), fisheries.csv (data)
-## After:  biology.csv, biomass.csv, catch.csv, cpue.csv, fatage.csv,
-##         length.comps.csv, likelihoods.csv, natage.csv, selectivity.csv,
-##         stats.csv, weight.comps.csv (output)
+## After:  biology.csv, biomass.csv, catch.csv, cpue.csv, f_aggregated.csv,
+##         f_annual.csv, f_season.csv, f_stage.csv, length.comps.csv,
+##         likelihoods.csv, natage.csv, selectivity.csv, stats.csv, summary.csv,
+##         weight.comps.csv (output)
 
 library(TAF)
 taf.library(FLR4MFCL)
@@ -22,7 +23,7 @@ like <- reading("likelihoods", read.MFCLLikelihood("model/test_plot_output"))
 wgtfit <- reading("weight fits", read.MFCLWgtFit("model/weight.fit"))
 
 # Read fisheries description
-fisheries <- read.taf("data/fisheries.csv")
+fisheries <- reading("fisheries description", read.taf("data/fisheries.csv"))
 
 # Likelihoods
 likelihoods <- summary(like)
@@ -82,10 +83,33 @@ names(selectivity)[names(selectivity) == "unit"] <- "fishery"
 names(selectivity)[names(selectivity) == "data"] <- "sel"
 selectivity <- selectivity[c("fishery", "age", "sel")]
 
-# Fishing mortality
-fatage <- as.data.frame(fm(rep))
-names(fatage)[names(fatage) == "data"] <- "f"
-fatage <- fatage[c("year", "season", "area", "age", "f")]
+# Fishing mortality: season
+f.season.all <- as.data.frame(fm_aggregated(rep))
+f.season.reg <- as.data.frame(fm(rep))
+f.season <- rbind(f.season.reg, f.season.all)
+names(f.season)[names(f.season) == "data"] <- "f"
+f.season$unit <- f.season$iter <- NULL
+
+# Fishing mortality: annual
+f.annual.all <- as.data.frame(seasonSums(fm_aggregated(rep)))
+f.annual.reg <- as.data.frame(seasonSums(fm(rep)))
+f.annual <- rbind(f.annual.reg, f.annual.all)
+names(f.annual)[names(f.annual) == "data"] <- "f"
+f.annual$unit <- f.annual$season <- f.annual$iter <- NULL
+
+# Fishing mortality: adult and juvenile
+p.adult <- mat(par)
+p.juven <-  1- p.adult
+f.adult <- aggregate(f~year+area, f.annual, weighted.mean, w=p.adult)
+f.juven <- aggregate(f~year+area, f.annual, weighted.mean, w=p.juven)
+f.adult$stage <- "adult"
+f.juven$stage <- "juvenile"
+f.stage <- rbind(f.adult, f.juven)[c("year", "area", "stage", "f")]
+
+# Fishing mortality: aggregate
+f.aggregate <- as.data.frame(AggregateF(rep))
+names(f.aggregate)[names(f.aggregate) == "data"] <- "f"
+f.aggregate <- f.aggregate[c("year", "season", "f")]
 
 # Numbers at age
 natage <- as.data.frame(popN(rep))
@@ -125,7 +149,10 @@ write.taf(biology, dir="output")
 write.taf(biomass, dir="output")
 write.taf(catch, dir="output")
 write.taf(cpue, dir="output")
-write.taf(fatage, dir="output")
+write.taf(f.season, dir="output")
+write.taf(f.annual, dir="output")
+write.taf(f.stage, dir="output")
+write.taf(f.aggregate, dir="output")
 write.taf(length.comps, dir="output")
 write.taf(likelihoods, dir="output")
 write.taf(natage, dir="output")
