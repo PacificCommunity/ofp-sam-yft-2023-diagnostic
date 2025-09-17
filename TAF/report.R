e795bc32a2f1ff9fc151abdbb91de00c50c6a324
Arni Magnusson <thisisarni@gmail.com>
2025-09-17 16:05:58 +1100

    Format tables after plotting

--- TAF/report.R
+++ TAF/report.R
@@ -10,18 +10,11 @@ library(lattice)
 
 mkdir("report")
 
-# Read results
+# Read tables
 biology <- read.taf("output/biology.csv")
 summary <- read.taf("output/summary.csv")
 f.stage <- read.taf("output/f_stage.csv")
 f.annual <- read.taf("output/f_annual.csv")
-
-# Format tables
-biology <- rnd(biology, 2:5, c(1,1,3,3))
-summary <- div(summary, 2:6, 10^c(6,3,3,3,3))
-summary <- rnd(summary, 2:8, c(0, 0, 0, 0, 0, 2, 2))
-biology <- format(biology)  # retain trailing zeros
-summary <- format(summary)  # retain trailing zeros
 
 # Plot adult and juvenile F
 taf.png("f_adult_juvenile_same_axes", width=2200, height=1400, res=300)
@@ -60,6 +53,13 @@ p <- xyplot(f~age|area, f.last.10, type="l", lwd=2, grid=TRUE, xlab="Age class",
 plot(p)
 dev.off()
 
-# Write TAF tables
+# Format tables
+biology <- rnd(biology, 2:5, c(1,1,3,3))
+summary <- div(summary, 2:6, 10^c(6,3,3,3,3))
+summary <- rnd(summary, 2:8, c(0, 0, 0, 0, 0, 2, 2))
+biology <- format(biology)  # retain trailing zeros
+summary <- format(summary)  # retain trailing zeros
+
+# Write tables
 write.taf(biology, dir="report")
 write.taf(summary, dir="report")
